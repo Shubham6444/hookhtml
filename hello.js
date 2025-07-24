69393deffb65d9f27600b79e78db455a9ed224d8
SHUBHAM MAURYA <72058313+Shubham6444@users.noreply.github.com>
2025-07-24 16:11:21 +0530

    Update and rename index.html to hello.js

---
+++ hello.js
@@ -0,0 +1,14 @@
+// hello.js
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, Node.js!\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
